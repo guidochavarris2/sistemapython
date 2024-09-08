79246ec5ae9f8f8a389d4c58e247c2f1f226888e
guidochavarris2 <lucanalopezguido@gmail.com>
2024-09-08 17:04:22 -0600

    hola

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'mi_secreto_super_secreto'  # Clave secreta para Flask (modificar para producción)
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/inventario_instituto'  # Cambiar según la configuración de tu BD
+    SECRET_KEY = 'f3e4b2c6e1a7b8d9c0a1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a2b3'  # Clave secreta generada
+    SQLALCHEMY_DATABASE_URI = 'mysql://ud9xqngyeotcnadg:M2LL7HY2UNl8NaDyMevu@b2y6evewizmxjaq6f4tf-mysql.services.clever-cloud.com:3306/b2y6evewizmxjaq6f4tf'  # Cambiar según la configuración de tu BD
     SQLALCHEMY_TRACK_MODIFICATIONS = False
